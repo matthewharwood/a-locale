aada5f909ee337f85ac7019b6500b6d6a1135c63
Matthew Harwood <mharwood@mharwood-macbookpro.roam.corp.google.com>
2017-08-12 21:09:05 -0700

    fix($compile): Still trying to fix import of module

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var build_locale_1 = require("./build-locale");
-exports.BuildLocale = build_locale_1.BuildLocale;
-//# sourceMappingURL=index.js.map
