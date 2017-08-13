8c3d65afce17db82398bc5f9b55d8b494d45f555
Matthew Harwood <mharwood@mharwood-macbookpro.roam.corp.google.com>
2017-08-12 20:56:30 -0700

    fix($build): Hopefully fixed build to support importing.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
 import { BuildLocale } from './build-locale';
 
 
-
-BuildLocale.getAngularApps()
-    .then(({apps}) => {
-      const appNames = BuildLocale.mapAppNames(apps);
-
-      if (appNames.length) {
-        return appNames;
-      } else {
-        return Error('No Applications were found');
-      }
-    })
-    .then(console.log);
-
+export {
+  BuildLocale
+};
